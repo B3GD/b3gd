be9f8fa9da9f1eea7d963a998b8647f0e1ecd7a5
squareverylong <squareverylong@proton.me>
2026-06-16 14:26:24 +0100

    Added charles

--- b3gd/chart_editor/strumline_container.gd
+++ b3gd/chart_editor/strumline_container.gd
@@ -1,11 +1,18 @@
 extends SplitContainer
 
+@export var strumline_node: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func update_strumlines():
+	for child in get_children():
+		if child.name.begins_with("_"):
+			continue
+		remove_child(child)
+	
